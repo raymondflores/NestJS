ea3239531b07b4bfd84b41033d3776e6ac88ef70
Raymond Flores <raymondfflores@gmail.com>
2020-12-09 15:58:48 -0600

    add jwt

--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
     return this.userRepository.signUp(authCredentials);
@@ -17,5 +22,10 @@ export class AuthService {
     if (!username) {
       throw new UnauthorizedException('Invalid Credentials');
     }
+
+    const payload: JwtPayload = { username };
+    const accessToken = this.jwtService.sign(payload);
+
+    return { accessToken };
   }
 }
