c62e6ac5f3809d966329ccab98792908d8cddfc5
Raymond Flores <raymondfflores@gmail.com>
2020-12-09 19:27:49 -0600

    finished graphql

---
+++ graphql-mongodb/src/student/student.resolver.ts
@@ -0,0 +1,27 @@
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql'
+import { CreateStudentInput } from './create-student.input'
+import { Student } from './student.entity'
+import { StudentService } from './student.service'
+import { StudentType } from './student.type'
+
+@Resolver((of) => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  students() {
+    return this.studentService.getStudents()
+  }
+
+  @Query((returns) => StudentType, { nullable: true })
+  student(@Args('id') id: string) {
+    return this.studentService.getStudent(id)
+  }
+
+  @Mutation((returns) => StudentType)
+  async createStudent(
+    @Args('createStudentInput') createStudentInput: CreateStudentInput,
+  ) {
+    return this.studentService.createStudent(createStudentInput)
+  }
+}
